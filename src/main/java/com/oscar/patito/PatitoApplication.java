b553ec255c97769227651aaee82b3ceae2ef357b
oscarph88 <oscarpadron.308@gmail.com>
2022-03-22 18:05:20 -0600

    First commit to patito

---
+++ src/main/java/com/oscar/patito/PatitoApplication.java
@@ -0,0 +1,13 @@
+package com.oscar.patito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatitoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PatitoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/oscar/patito/PatitoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oscar.patito;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatitoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
